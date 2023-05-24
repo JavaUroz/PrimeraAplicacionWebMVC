075261d08fe7d976f32b6df9f8fe70b1392ad35b
JavaUroz <urozjavier@gmail.com>
2023-05-24 13:31:09 -0300

    Apunte 5 completo

---
+++ Controllers/HolaMundoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text.Encodings.Web;
+using PrimeraAplicacionWebMVC.Models;
+
+namespace PrimeraAplicacionWebMVC.Controllers
+{
+    public class HolaMundoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Empleado empleado = new Empleado();
+            {
+                empleado.Id = 1;
+                empleado.Nombre = "Javier Uroz";
+                empleado.Salario = 10;
+            }
+            return View(empleado);
+        }
+    }
+}
---
+++ Models/Empleado.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimeraAplicacionWebMVC.Models
+{
+	public class Empleado
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public float Salario { get; set;}
+	}
+}
