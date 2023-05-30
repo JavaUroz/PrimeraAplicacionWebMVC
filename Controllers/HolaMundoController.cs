e8a88085ad90c1df7dd14ab6d77dddc1a40ba5c8
JavaUroz <urozjavier@gmail.com>
2023-05-30 12:51:25 -0300

    nueva actualizacion

--- Controllers/HolaMundoController.cs
+++ Controllers/HolaMundoController.cs
@@ -11,9 +11,14 @@ namespace PrimeraAplicacionWebMVC.Controllers
         {
             Empleado empleado = new Empleado();
             {
-                empleado.Id = 1;
-                empleado.Nombre = "Javier Uroz";
-                empleado.Salario = 10;
+                empleado.Id = 1;                
+                empleado.Nombre = "Roberto";
+                empleado.Apellido = "Perez";
+                empleado.Telefono = "(02473) 15 454545";
+                empleado.Email = "r.perez@email.com.ar";
+                empleado.Direccion = "Av. Pte. Irigoyen 1955";
+                empleado.Cuil = "20-22222222-5";
+				empleado.Salario = 100000;
             }
             return View(empleado);
         }
--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -4,6 +4,11 @@
 	{
 		public int Id { get; set; }
 		public string? Nombre { get; set; }
-		public float Salario { get; set;}
+		public string? Apellido { get; set; }
+		public string? Direccion { get; set; }
+		public string? Telefono { get; set; }
+		public string? Email { get; set; }
+		public string? Cuil { get; set; }
+		public float Salario { get; set; }
 	}
 }
